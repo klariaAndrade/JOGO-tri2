419fa0de4725c5ce34f4c0f42dedb660dbd816c8
Stella Flores <stellapereiraflores271@gmail.com>
2019-03-11 08:47:59 -0300

    primeiro script teste

--- Assets/scripts/simplemove.cs
+++ Assets/scripts/simplemove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SimpleMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position =new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
     }
 }
