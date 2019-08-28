a0a675c8d68808002641ac21f2b4ec6abd2ef74e
KlariaAndrade <Klariah2o@gmail.com>
2019-08-28 14:05:30 -0300

    Movimento do pinguim

---
+++ Assets/scripts/PatasCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatasCtrl : MonoBehaviour
+{
+    MoveToRef script;
+    public GameObject player;
+
+    Animator anim;
+
+    void Start()
+    {
+        script = player.GetComponent<MoveToRef>();
+        anim = player.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("plataforms") || other.gameObject.layer == LayerMask.NameToLayer("Obstacles")) {
+            script.noChao = true;
+            anim.SetInteger("state", 1);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("plataforms") || other.gameObject.layer == LayerMask.NameToLayer("Obstacles")) {
+            script.noChao = false;
+            anim.SetInteger("state", 2);
+        }
+    }
+}
